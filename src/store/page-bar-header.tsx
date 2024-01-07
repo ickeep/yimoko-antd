1ca860d39261f9f1919b101a3a20eb7889d1f2b4
ickeep <i@ickeep.com>
2024-01-08 00:45:28 +0800

    fix PageBarHeader 取值

--- src/store/page-bar-header.tsx
+++ src/store/page-bar-header.tsx
@@ -4,19 +4,19 @@ import { Breadcrumb, BreadcrumbProps, Divider, DividerProps, Space } from 'antd'
 import React, { ReactNode } from 'react';
 
 export const PageBarHeader = observer((props: PageBarHeaderProps) => {
-  const { breadcrumb = [], title, divider, children } = props;
+  const { breadcrumb, title, divider, children } = props;
   const scope = useExpressionScope();
   const { $config = {} } = scope ?? {};
 
-  const items: BreadcrumbProps['items'] = (breadcrumb ?? $config?.breadcrumb)?.map((item) => {
+  const items: BreadcrumbProps['items'] = (breadcrumb ?? $config?.breadcrumb)?.map((item: any) => {
     const { label, href, icon } = item;
     return { title: label, href, icon };
   });
 
-  const curTitle = title ?? $config?.title;
+  const curTitle = title ?? $config?.name;
 
   if (curTitle) {
-    items.push({ title: curTitle, href: '' });
+    items?.push({ title: curTitle, href: '' });
   }
 
   return (
