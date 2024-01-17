c2d0dcd7444e3d64345b5862ce84d14d425a28b9
ickeep <i@ickeep.com>
2024-01-18 00:49:11 +0800

    loading={store?.loading}

--- src/store/submit.tsx
+++ src/store/submit.tsx
@@ -1,3 +1,4 @@
+import { useCurStore } from '@yimoko/store';
 import { ButtonProps } from 'antd';
 import React from 'react';
 
@@ -5,11 +6,13 @@ import { Button } from '../base/button';
 
 export const Submit = (props: ButtonProps) => {
   const { onClick, onKeyDown, ...rest } = props;
+  const store = useCurStore();
 
   return (
     <Button
       children='提交'
       type='primary'
+      loading={store?.loading}
       {...rest}
       htmlType='submit'
     />
