730bd40b9ea37d6fd70b63c64d964c8f1f8b4964
ickeep <i@ickeep.com>
2024-04-17 23:47:27 +0800

    优化 RowActionSwitch 展示

--- src/store/action/row-action-switch.tsx
+++ src/store/action/row-action-switch.tsx
@@ -1,8 +1,8 @@
 import { useExpressionScope, observer } from '@formily/react';
 import { useCurStore } from '@yimoko/store';
-import { Spin } from 'antd';
+import { Button } from 'antd';
 import { get } from 'lodash-es';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { RowAction, RowActionProps } from './row-action';
 
@@ -17,31 +17,40 @@ export const RowActionSwitch = observer((props: Partial<RowActionProps>) => {
   const useRecord = props.record || $record;
   const isEnable = useRecord?.switch;
 
+  const actionProps = useMemo(() => {
+    const text = isEnable ? '禁用' : '启用';
+    const bProps: Omit<RowActionProps, 'store'> = {
+      size: 'small',
+      type: 'primary',
+      ghost: true,
+      children: children ?? text,
+      confirm: { content: `确定要${text} ${get(useRecord, labelKey) ?? ''} 吗？` },
+    };
+    if (isEnable) {
+      bProps.danger = true;
+    }
+
+    return { ...bProps, ...args };
+  }, [args, children, isEnable, labelKey, useRecord]);
+
   const { loading } = useCurStore() ?? {};
 
   // RowAction 强制重新渲染 否则 RowAction store 不更新
-  if (loading) return <Spin size='small' />;
+  if (loading) return <Button {...actionProps} />;
 
   if (isEnable) {
     return (
       <RowAction
-        type="primary"
-        ghost
-        danger
-        confirm={{ content: `确定要禁用 ${get(useRecord, labelKey) ?? ''} 吗？` }}
-        {...args}
+        {...actionProps}
         store={{ defaultValues: { [idKey]: '' }, api: $config?.api?.disableOne, ...store }}
-      >{children ?? '禁用'}</RowAction>
+      />
     );
   }
 
   return (
     <RowAction
-      type="primary"
-      ghost
-      confirm={{ content: `确定要启用 ${get(useRecord, labelKey) ?? ''} 吗？` }}
-      {...args}
+      {...actionProps}
       store={{ defaultValues: { [idKey]: '' }, api: $config?.api?.enableOne, ...store }}
-    >{children ?? '启用'}</RowAction>
+    />
   );
 });
