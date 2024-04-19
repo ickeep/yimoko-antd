23c73522364a2735cb3d9dd3542c79c152514adc
ickeep <i@ickeep.com>
2024-04-19 10:53:12 +0800

    BatchBarActionProps

--- src/store/batch-bar-actions.tsx
+++ src/store/batch-bar-actions.tsx
@@ -7,7 +7,7 @@ import { RowsActionDel } from './action/rows-action-del';
 import { RowsActionDisable } from './action/rows-action-disable';
 import { RowsActionEnable } from './action/rows-action-enable';
 
-export interface BatchBarActionProps extends Omit<SpaceProps, 'children'> {
+export interface BatchBarActionProps extends SpaceProps {
   isDel?: boolean
   isEnable?: boolean
   isDisabled?: boolean
@@ -15,7 +15,7 @@ export interface BatchBarActionProps extends Omit<SpaceProps, 'children'> {
 }
 
 export const BatchBarAction = observer((props: BatchBarActionProps) => {
-  const { isDel = true, actions, isEnable = true, isDisabled = true, style, ...rest } = props;
+  const { isDel = true, actions, isEnable = true, isDisabled = true, style, children, ...rest } = props;
 
   return (
     <Space wrap {...rest} style={{ width: '100%', marginBottom: 10, justifyContent: 'end', ...style }}>
@@ -23,6 +23,7 @@ export const BatchBarAction = observer((props: BatchBarActionProps) => {
       {isDisabled && <RowsActionDisable />}
       {isDel && <RowsActionDel />}
       {actions?.map((action, index) => <RowsAction key={index} {...action} />)}
+      {children}
     </Space>
   );
 });
